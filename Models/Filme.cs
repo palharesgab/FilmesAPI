550d9dac6f1df1a0010f9e7262b08e8b37f3a84e
Gabriel Palhares <pizzanigabriel@gmail.com>
2023-04-28 22:37:43 -0300

    Initial commit

---
+++ Models/Filme.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public double Nota { get; set; }
+    }
+}
---
+++ Services/FilmeService/IFilmeService.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmesAPI.Services.FilmeService
+{
+    public interface IFilmeService
+    {
+        Task<List<Filme>> RetornaListaFilmes();
+        Task<Filme?> RetornaFilme(int id);
+        Task<List<Filme>> AdicionaFilme(Filme filme);
+        Task<List<Filme>?> AlteraFilme(int id, Filme request);
+        Task<List<Filme>?> DeletaFilme(int id);
+    }
+}
